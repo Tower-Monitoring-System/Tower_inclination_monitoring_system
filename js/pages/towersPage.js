fba24f7b65c26efe36cf060ed0c7661a899f1e62
NGUYENHIEN <trannguyenhien29085@gmail.com>
2026-08-24 08:17:25 +0700

    update

--- js/pages/towersPage.js
+++ js/pages/towersPage.js
@@ -103,6 +103,7 @@ export class TowersPage {
       emptyTitle: byId("towerEmptyTitle"),
       emptyDescription: byId("towerEmptyDescription"),
       content: byId("towerMonitoringContent"),
+      trendCount: byId("towerTrendCount"),
       trendLoading: byId("towerTrendLoading"),
       vectorLoading: byId("towerVectorLoading"),
       vectorX: byId("towerVectorX"),
