90e0c814c9ecdbd4b7b9243833f0bd628581e3b0
NGUYENHIEN <trannguyenhien29085@gmail.com>
2026-08-26 07:33:43 +0700

    update

--- js/core/supabaseConfig.js
+++
@@ -1,7 +0,0 @@
-export const SUPABASE_CONFIG = Object.freeze({
-  // Public browser configuration. These values are safe to expose in a client app.
-  // NEVER put a Supabase secret key / service_role key in this file.
-  url: "https://zyimagstocernfygfqzh.supabase.co",
-  publishableKey: "sb_publishable_CR7cgx-KHSFzpGvMfhGbPA_rL0VxsGA",
-  usernameLoginFunction: "username-login"
-});
