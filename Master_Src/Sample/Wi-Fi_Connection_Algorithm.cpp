37b04c05114a7c8aa83f5851927254a00f515ca9
NGUYENHIEN <trannguyenhien29085@gmail.com>
2026-08-26 08:01:27 +0700

    update

--- Master_Src/Sample/Wi-Fi_Connection_Algorithm.cpp
+++ Master_Src/Sample/Wi-Fi_Connection_Algorithm.cpp
@@ -1,10 +1,15 @@
 #include <WiFi.h>
 const char *ssid = "TINIHI"; // TINIHI&&ESP32_Transmit
 const char *password = "thanhnguyen201077";   // thanhnguyen201077&&12345678
+const uint8_t ledPins[] = {7, 6, 5, 4}; 
+const uint8_t numLeds = 4;
 
 // --- KHAI BAO BIEN THOI GIAN ---
+unsigned long previousMillis_Signal = 0;     
 unsigned long previousMillis_Disconnect = 0; 
 unsigned long previousMillis_Restart = 0; 
+
+const uint16_t interval_SignalLevel = 500;
 const uint16_t interval_disconnect = 20000;
 const uint16_t interval_restartEsp32 = 50000;
 
@@ -16,6 +21,7 @@ enum WifiState {
 volatile WifiState currentWifiState = WS_DISCONNECTED;
 
 // --- Prototype ---
+void showSignalLevel(int rssi);
 void ConnectingEffect();
 void LostConnectEffect();
 void Disconnect();
@@ -62,6 +68,12 @@ void WiFiEvent(WiFiEvent_t event) {
 
 void setup() {
   Serial.begin(115200);
+
+  for (uint8_t i = 0; i < numLeds; i++) {
+    pinMode(ledPins[i], OUTPUT);
+    digitalWrite(ledPins[i], HIGH);
+  }
+
   WiFi.onEvent(WiFiEvent);
   WiFi.mode(WIFI_STA);
   WiFi.begin(ssid, password);
@@ -74,6 +86,8 @@ void loop() {
       break;
 
     case WS_CONNECTED: {
+      int rssi = WiFi.RSSI();
+      showSignalLevel(rssi);
       break;
     }
 
@@ -103,12 +117,44 @@ void RestartESP32() {
   }
 }
 
+// --- HIEN THI CONG SUAT MUC SONG ---
+void showSignalLevel(int rssi) {
+  unsigned long currentMillis = millis();
+
+  if(currentMillis - previousMillis_Signal >= interval_SignalLevel) {
+    Serial.printf(rssi);
+    for (uint8_t i = 0; i < numLeds; i++) digitalWrite(ledPins[i], HIGH);
+    if (rssi <= -90) { // Lost Connect
+      for (uint8_t i = 0; i < numLeds; i++) digitalWrite(ledPins[i], HIGH);
+    }
+
+    if (rssi > -90 && rssi <= -70) digitalWrite(ledPins[0], LOW); // Connect Yeu    
+      else if (rssi > -70 && rssi <= -50) {
+        for(uint8_t i = 0; i < 2; i++) digitalWrite(ledPins[i], LOW); // Connect TB 
+    } else if (rssi > -50 && rssi <= -30) {
+        for(uint8_t i = 0; i < 3; i++) digitalWrite(ledPins[i], LOW); // Connect Tot 
+    } else if (rssi > -30) {
+        for(uint8_t i = 0; i < 4; i++) digitalWrite(ledPins[i], LOW); // Connect Cuc Manh
+    }   
+
+    previousMillis_Signal = currentMillis;
+  }
+}
+
 // --- HIEU UNG LED "LOADING" ---
 void ConnectingEffect() {
-
+  static uint8_t Positions_Led = 0;
+  for(uint8_t i = 0; i < numLeds; i++) digitalWrite(ledPins[i], HIGH);
+  digitalWrite(ledPins[Positions_Led], LOW);
+  Positions_Led++;
+  if(Positions_Led >= numLeds) Positions_Led = 0;
+  delay(150);
 }
 
 // --- HIEU UNG MAT KET NOI ---
 void LostConnectEffect() {
-
+  for(uint8_t i = 0; i < numLeds; i++) digitalWrite(ledPins[i], HIGH);
+  delay(300);
+  for(uint8_t i = 0; i < numLeds; i++) digitalWrite(ledPins[i], LOW);
+  delay(300);
 }
